9fb2c59c672cb1fad467c968d70c8d7b5040dfd4
Mathieu Scotet <mscotet.lmit@gmail.com>
2023-02-24 17:14:07 +0100

    skot 1.2.6

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,5 +2,5 @@ object Versions {
     const val group = "tech.skot.libraries.sk-map"
     const val frameworkGroup = "tech.skot"
     const val library = "0.0.20"
-    const val framework = "1.2.5"
+    const val framework = "1.2.6"
 }
